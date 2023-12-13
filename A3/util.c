7b95f61eb9395886dcc9f82a7552c201f2db57c3
OskarLinke <ingenandre@OskarsPC>
2023-12-13 20:46:16 +0100

    implemented distance

--- A3/util.c
+++ A3/util.c
@@ -4,11 +4,21 @@
 #include <assert.h>
 
 double distance(int d, const double *x, const double *y) {
-  assert(0);
+  
+  double result; 
+  //simply using the euclidian distance formula
+  for (i = 0; i++; i<d){
+    result = result + ((x[i] - y[i])**2); 
+  }
+  result = sqrt(result); 
+
+  return result; 
 }
 
 int insert_if_closer(int k, int d,
                      const double *points, int *closest, const double *query,
                      int candidate) {
+
+
   assert(0);
 }
