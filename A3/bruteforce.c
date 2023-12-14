bed7d8e6048071ee41b3adeb0cd71e583f72e80a
OskarLinke <ingenandre@OskarsPC>
2023-12-14 16:40:48 +0100

    implemented bruteforce.c

--- A3/bruteforce.c
+++ A3/bruteforce.c
@@ -1,9 +1,23 @@
 #include "bruteforce.h"
 #include "util.h"
+#include "io.h"
+#include "util.h"
 #include <stdlib.h>
 #include <assert.h>
 
 int* knn(int k, int d, int n, const double *points, const double* query) {
-  assert(0);
+  int* closest = malloc(k*sizeof(int));
+  for (int i = 0; i < k; i++) {
+    closest[i] = -1;
+  }
+
+  for (int i = 0; i < n; i++) {
+    insert_if_closer(k, d, points, closest, query, i);
+  }
+
+  return closest; 
 }
+
+
+
 
