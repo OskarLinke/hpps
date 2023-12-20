5ab29e061245d47da48e647f7aea841ceb8f2a73
OskarLinke <ingenandre@OskarsPC>
2023-12-20 17:05:54 +0100

    added the files

---
+++ a4/id_query_naive.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <stdint.h>
+#include <errno.h>
+#include <assert.h>
+
+#include "record.h"
+#include "id_query.h"
+
+struct naive_data {
+  struct record *rs;
+  int n;
+};
+
+struct naive_data* mk_naive(struct record* rs, int n) {
+  // TODO
+  assert(0);
+}
+
+void free_naive(struct naive_data* data) {
+  // TODO
+  assert(0);
+}
+
+const struct record* lookup_naive(struct naive_data *data, int64_t needle) {
+  // TODO
+  assert(0);
+}
+
+int main(int argc, char** argv) {
+  return id_query_loop(argc, argv,
+                    (mk_index_fn)mk_naive,
+                    (free_index_fn)free_naive,
+                    (lookup_fn)lookup_naive);
+}
